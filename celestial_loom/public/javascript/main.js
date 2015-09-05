f4320f26e41b09f376ee2b1d70388a40d12d9c6d
Beau Davenport <davenportbeau@gmail.com>
2015-09-05 21:42:37 +0000

    Add paypal button, tweak styles

--- celestial_loom/public/javascript/main.js
+++ celestial_loom/public/javascript/main.js
@@ -1,4 +1,8 @@
+---
+---
 $(function() {
+	var numberOfParagraphs = {{ site.blog_paragraphs }};
+	
 	var parseRSS = function(url, successCallback, errorCallback) {
 	  $.ajax({
 	    url: document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=' + encodeURIComponent(url),
@@ -12,21 +16,32 @@ $(function() {
 	  });
 	}
 	
-	parseRSS('https://nikiastro.wordpress.com/feed/', function(feed) {
-		var lastEntry = feed.entries[0];
-		var $entryContent = $(lastEntry.content);
-		var $blogContent= $entryContent.filter("p").slice(0,2);
-		$('.blog-content').html($blogContent);
-		$('.blog-content').after("<div class='blog-title'><h3>" + lastEntry.title +
-									" <small>" + lastEntry.publishedDate.split(" ").slice(0,4).join(" ") +
-									"</small></h3>" + "<h4><a href='" + lastEntry.link + 
-									"'> read more...</a></h4></div>");
-		console.log(lastEntry);
-
-	}, function() {
+	var renderContent = function(entry, content) {
+		$('#blog-content').html(content);
+		$('#blog-title').html("<div class='blog-title'><h3>" + entry.title +
+									"</h3><small>" + entry.publishedDate.split(" ").slice(0,4).join(" ") +
+									"</small></div>"
+									);
+		$('#blog-footer').html("<div class='blog-title'><small>...continue reading on </small><a href='" + 
+									entry.link +
+									"'>AstroSynergistics</a><small>, the astrology blog of Nikki Davenport</small></div>"
+									);
+	}
+	
+	var renderError = function() {
 		$('.blog-content').html(
 			"<p>Can't load blog content. View more posts here: " +
 			"<a href='http://astrosynergistics.blogspot.com'>Astrosynergistics</a></p>"
 			);
+	}
+	
+	parseRSS('https://nikiastro.wordpress.com/feed/', function(feed) {
+		var lastEntry = feed.entries[0];
+		var $entryContent = $(lastEntry.content);
+		var $blogContent= $entryContent.filter("p, ul").slice(0,numberOfParagraphs);
+		console.log(lastEntry.content);
+		renderContent(lastEntry, $blogContent);
+	}, function() {
+		renderError();
 	});
 });
